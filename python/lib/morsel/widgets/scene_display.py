e1de038c718eb41e84bca0eb65a1fe7537539b1a
Ralf Kaestner <ralf.kaestner@gmail.com>
2012-01-04 08:48:47 +0000

    Fixed SceneDisplay margin

--- python/lib/morsel/widgets/scene_display.py
+++ python/lib/morsel/widgets/scene_display.py
@@ -5,7 +5,7 @@ from label import Label
 
 class SceneDisplay(Label):
   def __init__(self, gui, name = "SceneDisplay", anchor = ["Center", "Bottom"],
-      origin = [0, -1], margin = [0.5, 0.3], hidden = True, **kargs):
+      origin = [0, -1], margin = [0.03, 0.02], hidden = True, **kargs):
     Label.__init__(self, gui, name, anchor = anchor, origin = origin,
       margin = margin, hidden = hidden, **kargs)
   
